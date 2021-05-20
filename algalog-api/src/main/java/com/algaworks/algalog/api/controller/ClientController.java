7ecd127996c99f92d5c837e18ff9e4bf4980efe0
Robertotheto <robertodossantosfilho1001@gmail.com>
2021-05-19 20:51:13 -0400

    Jakarta Persistence

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +12,13 @@ import com.algaworks.algalog.domain.models.Client;
 
 @RestController
 public class ClientController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clients")
-	public List<Client> listar() {
-		
-		var client1 = new Client();
-		client1.setId(1L);
-		client1.setNome("Roberto");
-		client1.setEmail("roberto@gmail.com");
-		client1.setTelefone("66 9 99523354");
-		
-		var client2 = new Client();
-		client2.setId(2L);
-		client2.setNome("lucas");
-		client2.setEmail("lucas@gmail.com");
-		client2.setTelefone("66 9 99520000");
-		
-		return Arrays.asList(client1, client2);	
+	public List<Client> list() {
+		return manager.createQuery("from Client", Client.class)
+				.getResultList();
 	}
 }
--- algalog-api/src/main/java/com/algaworks/algalog/domain/models/Client.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/models/Client.java
@@ -1,12 +1,25 @@
 package com.algaworks.algalog.domain.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "clients")
 public class Client {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
