62677369858552cee8ef1fbc2d1cb511970c2d05
Robertotheto <robertodossantosfilho1001@gmail.com>
2021-05-21 20:49:19 -0400

    DTO

---
+++ algalog-api/src/main/java/com/algaworks/algalog/api/model/DeliveryModel.java
@@ -0,0 +1,22 @@
+package com.algaworks.algalog.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.algaworks.algalog.domain.models.StatusDelivery;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class DeliveryModel {
+	private Long id;
+	private String nomeClient;
+	private RecipientModel recipient;
+	private BigDecimal rate;
+	private StatusDelivery status;
+	private OffsetDateTime dateOrder;
+	private OffsetDateTime dateFinish;
+}
